4f56926c36c489e9c2f6d28a0eb010e360e7c23f
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2022-02-11 10:34:22 +0900

    存在しないファイル名だった場合処理を終了させる

--- execute_question1.rb
+++ execute_question1.rb
@@ -1,5 +1,15 @@
 require "csv"
 
 puts "読み込みたいファイル名を拡張子抜きで入力してください。 例)log.csvというファイルを入力したい場合は「log」と入力してください。"
-file_name = gets.chomp
-puts File.exist? "#{file_name}.csv"
+# file_name = gets.chomp
+file_name = "log"
+
+unless File.exist? "#{file_name}.csv"
+  puts "#{file_name}.csvは存在しません。ファイル名を確認してください。"
+  return
+end
+# => ["Ruby", "1995"]
+#    ["Rust", "2010"]
+
+# ファイルから一度に
+# p CSV.read("#{file_name}.csv")
