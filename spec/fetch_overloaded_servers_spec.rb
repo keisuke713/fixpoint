bb2d9e34b36e74a0f414eb05d56b4eacff9974a5
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2022-02-11 19:13:54 +0900

    設問3をみたすロジックを追加

--- spec/fetch_overloaded_servers_spec.rb
+++ spec/fetch_overloaded_servers_spec.rb
@@ -1,13 +1,6 @@
 require "../src/fetch_overloaded_servers"
 
 RSpec.describe "過負荷のサーバーを解析する" do
-  context "1回でもタイムアウトしたら故障とみなす" do
-    it "test" do
-      expect(fetch_overloaded_servers([],1,1)).to eq [1]
-    end
-  end
-  context "2回以上タイムアウトしたら故障とみなす" do
-  end
   it "指定の回数ログが出力されなかった場合" do
     input = [
       ["20201019133124", "10.20.30.1/16", "-"],
--- src/fetch_overloaded_servers.rb
+++ src/fetch_overloaded_servers.rb
@@ -1,12 +1,32 @@
 require "pry"
+TIMEOUT_MESSAGE = "-"
 
-def fetch_overloaded_servers(logs, times, average)
+def fetch_overloaded_servers(logs, limit, average)
+  servers_queue = {}
+  response_sums = {}
   result = []
 
   logs.each do |log|
     time = log[0]
     address = log[1]
     response = log[2]
+
+    next if response == TIMEOUT_MESSAGE
+
+    response_sums.store(address, response_sums.fetch(address, 0) + response.to_i)
+    if servers_queue.has_key?(address)
+      servers_queue.fetch(address).push({"time" => time, "response" => response.to_i})
+    else
+      servers_queue.store(address, [{"time" => time, "response" => response.to_i}])
+    end
+    if servers_queue.fetch(address).size > limit
+      oldest_data = servers_queue.fetch(address).shift
+      response_sums.store(address, response_sums.fetch(address) - oldest_data.fetch("response"))
+    end
+
+    next if servers_queue.fetch(address).size < limit
+    current_average = (response_sums.fetch(address) / limit).floor
+    result.push({"address" => address, "from" => servers_queue.fetch(address)[0].fetch("time"), "to" => time}) if current_average >= average
   end
 
   result
