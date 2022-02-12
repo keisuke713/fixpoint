a2bed53a5ba80260e28f297a2a7db9f28aa6aaf0
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2022-02-12 15:47:24 +0900

    log内でaddressのインスタンス変数を持つのをやめる

--- src/log.rb
+++ src/log.rb
@@ -1,26 +1,30 @@
 class Log
   TIMEOUT_MESSAGE = "-"
-  attr_reader :time, :response
+  attr_reader :time, :network, :host, :subnet, :response
 
   def initialize(time, address, response)
     @time = time
-    @address = address
     @response = response
+    set_network_and_host_and_subnet(address)
   end
 
   def is_timeout?
     response == TIMEOUT_MESSAGE
   end
 
-  def network
-    @address.network
-  end
-
-  def host
-    @address.host
-  end
-
   def address
-    @address.to_string
+    [
+      [network, host].join("."), subnet
+    ].join("/")
+  end
+
+  private
+
+  def set_network_and_host_and_subnet(address)
+    tmp = address.split(/\.|\//)
+    @subnet = tmp[-1].to_i
+    partition = subnet / 8
+    @network = tmp[0...partition].join(".")
+    @host = tmp[partition...-1].join(".")
   end
 end
