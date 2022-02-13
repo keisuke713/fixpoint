368ad0f7306714070dc7231e6c53f6dc09a8cc4d
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2022-02-13 10:02:42 +0900

    ファイル名を入力出来るように修正

--- main.rb
+++ main.rb
@@ -15,8 +15,7 @@ QUESTIONS = [
 
 def main
   puts "読み込みたいファイル名を拡張子抜きで入力してEnterを押してください。 例)log.csvというファイルを入力したい場合は「log」と入力してください。"
-  # file_name = gets.chomp
-  file_name = "sample"
+  file_name = gets.chomp
 
   unless File.exist? "#{file_name}.csv"
     puts "#{file_name}.csvは存在しません。ファイル名を確認してください。"
